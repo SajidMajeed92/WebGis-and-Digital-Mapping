91b991ca08440fc7f9ca8ae5eb41c016a7e01d72
sajidmajeed92 <sajidmajeed.ist@gmail.com>
2023-10-05 04:15:13 -0700

    Uploaded Data

--- map.py
+++ map.py
@@ -8,6 +8,12 @@ m = folium.Map(location=[30.3753, 69.3451], zoom_start=6)
 # Global tooltip
 tooltip = 'Click For More Info'
 
+# Create custom marker icon
+logoIcon = folium.features.CustomIcon('data/logo.png', icon_size=(50, 50))
+
+
+# Vega data
+vis = os.path.join('data', 'vis.json')
 
 # Create markers
 folium.Marker([24.8607, 67.0011],
@@ -29,6 +35,14 @@ folium.Marker([24.952101, 67.127094],
               tooltip=tooltip,
               icon=folium.Icon(color='green', icon='leaf')).add_to(m),
 
+folium.Marker([24.946437,67.1330186],
+              popup='<strong>IST Karachi Location</strong>',
+              tooltip=tooltip,
+              icon=logoIcon).add_to(m),
+
+
+folium.Marker([26.946437,68.1330186],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(vis)), width=550, height=300))).add_to(m)
 
 
 # Generate a map using Folium
